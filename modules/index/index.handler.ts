e7599e419d12240f5a64edd144d6178e404b70bd
ynwd <10122431+ynwd@users.noreply.github.com>
2025-04-24 05:04:54 +0700

    chore: update index.handler

--- modules/index/index.handler.ts
+++ modules/index/index.handler.ts
@@ -1,14 +1,10 @@
 import { Context, HttpRequest } from "fastro/core/server/types.ts";
-import { indexService } from "@app/modules/index/index.service.ts";
 import { getSession } from "@app/utils/session.ts";
 
 export async function indexHandler(req: HttpRequest, ctx: Context) {
   const ses = await getSession(req, ctx);
   const isLogin = ses?.isLogin;
-  // const avatar_url = ses?.avatar_url;
-  // const html_url = ses?.html_url;
 
-  // Redirect to home page if user is already logged in
   if (isLogin) {
     return new Response(null, {
       status: 302,
@@ -18,6 +14,10 @@ export async function indexHandler(req: HttpRequest, ctx: Context) {
     });
   }
 
-  const indexData = indexService();
-  return ctx.render(indexData);
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "https://social.fastro.dev",
+    },
+  });
 }
