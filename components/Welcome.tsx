37bcd0ed4b70d4fe5100c7e2ff7a88b736df03d3
ynwd <10122431+ynwd@users.noreply.github.com>
2025-05-05 06:27:58 +0700

    chore: update Welcome and bolt

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -4,8 +4,8 @@ export default function Welcome() {
   return (
     <div className={`m-3 text-center space-y-3 `}>
       <div className="flex justify-center">
-        <div className="text-white-400 border border-gray-600/50 bg-gray-900 rounded-full p-3 animate-spin hover:animate-pulse transition-all  duration-[5000ms]">
-          <BoltSvg width="40" height="40" />
+        <div className="text-white-400 border border-gray-600/50 bg-gray-900 rounded-full p-2 sm:p-3 animate-spin hover:animate-pulse transition-all duration-[5000ms]">
+          <BoltSvg width="24" height="24" className="sm:w-8 sm:h-8" />
         </div>
       </div>
       <p className="text-base sm:text-xl opacity-80">
