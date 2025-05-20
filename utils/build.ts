158667e09ae8fb69ff6b7f261b1a32a5412cf105
ynwd <10122431+ynwd@users.noreply.github.com>
2025-05-20 11:01:38 +0700

    chore(build): refactor index-plugin

--- utils/build.ts
+++ utils/build.ts
@@ -1,23 +1,20 @@
 // deno-lint-ignore-file no-explicit-any
-import { h } from "preact";
 import * as esbuild from "npm:esbuild@0.25.3";
-import { renderToString } from "preact-render-to-string";
-import { indexLayout } from "../modules/index/index.layout.tsx";
-import Index from "../modules/index/index.page.tsx";
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.0";
 import postcss from "postcss";
 import tailwindCss, { Config } from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import { TailwindPluginOptions } from "@app/middlewares/tailwind/types.ts";
+import { indexPlugin } from "./index-plugin.ts";
 
-interface BuildResult {
+export interface BuildResult {
   errors: esbuild.Message[];
   warnings: esbuild.Message[];
   outputFiles?: esbuild.OutputFile[];
 }
 
-interface PluginBuild {
+export interface PluginBuild {
   onEnd(callback: (result: BuildResult) => void | Promise<void>): void;
 }
 
@@ -73,68 +70,6 @@ async function processCss(staticDir: string) {
   }
 }
 
-const htmlPlugin = {
-  name: "html",
-  setup(build: PluginBuild) {
-    build.onEnd(async (result: BuildResult) => {
-      if (result.errors.length > 0) return;
-
-      try {
-        const pageData = {
-          user: "Example User",
-          title: "Join the Conversation",
-          description: "Today's ideas become tomorrow's innovations",
-          github_auth: "https://web.fastro.dev/auth/github/signin",
-          base_url: Deno.env.get("ENV") === "DEV"
-            ? ""
-            : "https://web.fastro.dev",
-          apiBaseUrl: Deno.env.get("API_BASE_URL") || "https://web.fastro.dev",
-          share_base_url: Deno.env.get("SHARE_BASE_URL") ||
-            "https://social.fastro.dev",
-          avatar_url: "",
-          isLogin: false,
-          author: "",
-          message: "",
-        };
-
-        const metaData = {
-          title: pageData.title,
-          description: pageData.description,
-          image: "https://social.fastro.dev/img/social.jpeg",
-        };
-
-        let fullHtml = `<!DOCTYPE html>${
-          renderToString(
-            indexLayout({
-              children: h(Index, { data: pageData }),
-              data: metaData,
-              nonce: "random-nonce-value",
-            }),
-          )
-        }`;
-
-        // Serialize the data for client-side hydration
-        const serializedData = JSON.stringify(pageData).replace(
-          /</g,
-          "\\u003c",
-        );
-        const scripts =
-          `<script id="__DATA__" type="application/json">${serializedData}</script>` +
-          `<script type="module" src="/js/bundle.js?v=${
-            new Date().getTime()
-          }"></script>`;
-
-        fullHtml = fullHtml.replace("</body>", `${scripts}</body>`);
-
-        await Deno.writeTextFile("public/index.html", fullHtml);
-        console.log("HTML file written successfully");
-      } catch (error) {
-        console.error("Error during rendering:", error);
-      }
-    });
-  },
-};
-
 try {
   const cwd = Deno.cwd();
   const configPath = `${cwd}/deno.json`;
@@ -161,7 +96,7 @@ Sitemap: https://social.fastro.dev/sitemap.xml`;
     jsxFactory: "h",
     jsxFragment: "Fragment",
     plugins: [
-      htmlPlugin,
+      indexPlugin,
       ...denoPlugins({
         configPath,
       }),
